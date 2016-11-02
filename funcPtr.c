386afee73007b8f0aa957a966e95ae1c2bc837d2
Rufian55 <kearnsc@oregonstate.edu>
2016-11-02 18:59:05 -0400

    Function pointers demystified.

---
+++ funcPtr.c
@@ -0,0 +1,29 @@
+/****************************************************************************************
+* funcPtr.c demonstrates how function pointers are declared nd used.
+* Compile with "gcc funcPtr.c -o funky -g -Wall".
+*****************************************************************************************/
+#include <stdio.h>
+
+// Function Prototype.
+int addOne(int inputArg);
+
+int main() {
+
+	int(*fpArg)(int) = addOne;
+/*     ^     ^    ^        ^
+	  |	   |    |		  |
+	  |	   |    |        Sets @ of Addone() to fpArg.
+	  |     |    Argument - same as addOne().
+	  |     Declared function pointer fpArg.
+	  Return. */
+
+	printf("10 + 1 = %d\n", fpArg(10));
+	// Use ptr to addOne()	  ^
+
+	return 0;
+}
+
+// The AddOne function.
+int addOne(int input) {
+	return input + 1;
+}
