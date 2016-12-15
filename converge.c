ce0c1e68c6a7648e608c3ef5da82c77d4bc1abed
Rufian55 <kearnsc@oregonstate.edu>
2016-12-15 10:58:12 -0500

    Converge

---
+++ converge.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+void converge(char *target, char * source);
+
+int main() {
+	char target[80] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; //30.
+	converge(target, "This is a test of converge().");
+	printf("Final string: %s\n", target);
+
+	return 0;
+}
+
+
+// Copies from both ends, converging in the middle.
+void converge(char *target, char *source) {
+	int i, j;
+	printf("%s\n", target);
+
+	// i set to incement from 0, j set to decement from 30.
+	for (i = 0, j = strlen(source); i <= j; i++, j--) {
+		target[i] = source[i];
+		target[j] = source[j];
+		printf("%s\n", target);
+	}
+}
