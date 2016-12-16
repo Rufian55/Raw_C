fef0b0be99b6df91ec0675813253c5c8b53f9e43
Rufian55 <kearnsc@oregonstate.edu>
2016-12-16 10:19:49 -0500

    With comments

--- strstrC.c
+++ strstrC.c
@@ -15,19 +15,28 @@ int main() {
 	return 0;
 }
 
-/* Return index of first match of s2 in s1. */
+/* Return index of first match of substring s2 in string s1. */
 int findSubString(char *s1, char *s2) {
 	int t;
+	// Declare 2 char pointers.
 	char *p, *p2;
+	// Iterate through s1 with t as index.
 	for (t = 0; s1[t]; t++) {
+		// Set p to each s1 element's memory address.
 		p = &s1[t];
+		// Set p2 to point at string s2.
 		p2 = s2;
+		/* Check each char of p2 to make sure there is another char and if it
+		   matches, increment to the next char, checking for a match on each pass. */
 		while (*p2 && *p2 == *p) {
 			p++;
 			p2++;
 		}
+		/* Check to see if all of s2 was consumed, if so, we have a match, and 
+		   now know the starting index (t) of s2 within s1. */
 		if (!*p2)
 			return t;
 	}
+	// No match...
 	return -1;
 }
