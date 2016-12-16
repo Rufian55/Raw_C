d5706b6bddb8891481539232f6d4ac078ae48fb7
Rufian55 <kearnsc@oregonstate.edu>
2016-12-16 11:41:24 -0500

    Recursion and Iteration simple comarison

--- recurse.c
+++ recurse.c
@@ -1,8 +1,9 @@
 /****************************************************************************************
-* findChar.c demonstrates how finding a single char within a string.
-* Compile with "gcc findChar.c -o findChar -g -Wall".
+* recuse.c demonstrates recusion and iteration...
+* Compile with "gcc recurse.c -o recurse -g -Wall".
 *****************************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 
 // Prototype.
 int R_factorial(int);
@@ -11,7 +12,14 @@ int I_factorial(int);
 int main() {
 	int a,b,f;
 	printf("Enter an int to be factored.\n");
-	fgets(f, sizeof(f), stdin);
+	scanf("%d", &f);
+
+	f = (int)f;// Just in case a char is entered.
+
+	if (f < 1 || f > 13) {
+		printf("Factorial man! ints between 1 and 13 inclusive only!\n");
+		exit(EXIT_FAILURE);
+	}
 
 	a = R_factorial(f);
 	b = I_factorial(f);
@@ -24,10 +32,10 @@ int main() {
 /* Recursive */
 int R_factorial(int n) {
 	int answer;
-	if (n == l)
+	if (n == 1)
 		return 1;
 
-	answer = R_factorial(n - l) * n;
+	answer = R_factorial(n - 1) * n;
 	return(answer);
 }
 
